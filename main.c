a8041116b4033fd19a669ca3053a46298af58be5
mj0717 <a81930875@gmail.com>
2022-10-04 17:56:07 +0900

    5-3

--- main.c
+++ main.c
@@ -4,18 +4,20 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x;
+    char c;
+	int num=0; 
 	
-	printf("정수 하나를 입력하시오 :");
-	scanf("%d", &x);
+	printf("문자열을 입력하세요: ");
+	while((c=getchar())!='\n')//입력 문자가 개형문자가 나올 때까지 반복 
+	{
+	 if (c>='0'&&c<='9')
+		//입력된 글자가 숫자인가
+		num=num+1;
+		//참이면 하나를 센다. 
+		
+	}
 	
-	if (x>0)
-		printf("양수입니다.\n");
-	else if (x<0)
-		printf("음수입니다.\n");
-	else
-		printf("0입니다.\n");
-
+	printf("숫자의 개수는 %i개 입니다.", num);
 		
 	
 	
