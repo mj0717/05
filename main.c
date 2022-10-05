1e8bdec29485871b38db4abb45ab62273d36b784
mj0717 <a81930875@gmail.com>
2022-10-05 13:01:35 +0900

    5-6

--- main.c
+++ main.c
@@ -4,17 +4,33 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-    int sum=0;
+	
+	int answer=59;
 	int x;
-	int i;
+	int trial;
+	;
+	do
+	{
+		printf("Guess a number :");//입력해라 문구 출력
+		scanf("%i", &x);//입력을 받음
+		
+		
+		if (x<59)
+			printf("low!\n");
+		else if (x>59)
+			printf("high!\n");
+		else
+			printf("Congratulation!\n");
+		trial++;
+		//입력숫자가 정답보다 큰지 작은지 출력	
+			
+	}while (x!=59);
 	
-	printf("정수를 입력하시오 :");
-	scanf("%d", &x);
+	printf("Number of trial=%i\n", trial);
 	
-	for (i=0;i<=x;i++)
-		sum=sum+i;
+
 	
-	printf("더하기 결과는 %i입니다.\n", sum);
+   
 	
 		
 	
